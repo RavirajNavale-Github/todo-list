5a69269b49d5f9cbd9d501bf78205b8c9ef04667
RavirajNavale-Github <165894463+RavirajNavale-Github@users.noreply.github.com>
2024-04-23 04:23:30 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import TodoContainer from './Components/TodoContainer'
+import { Bars } from "react-loader-spinner";
+
+
+const App = () => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    // useEffect(() => {
+    //     fetch("https://jsonplaceholder.typicode.com/todos")
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             setTimeout(() => {
+    //                 setData(data)
+    //                 setLoading(true);
+    //             }, 1000);
+    //         });
+    // }, []);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("https://ravirajnavale-mock-server-app.onrender.com/todo");
+                const data = await response.json();
+                setData(data);
+                setLoading(true);
+            } catch (err) {
+                console.error("Error fetching data:", err);
+            }
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            {loading ? (
+                <TodoContainer fakeTodosArr={data} />
+            ) : (
+                <Bars height="180" width="180" color="#4fa94d" ariaLabel="bars-loading" visible={true} />
+            )}
+
+        </div>
+    )
+}
+
+export default App
